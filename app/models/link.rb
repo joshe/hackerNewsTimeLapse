ac401881a7df0bb9912f126e71e264abfa0eb675
Josh Evnin <jevnin@gmail.com>
2011-12-10 12:24:00 -0600

    Implemented Hacker News style, jquery slider

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,12 @@ class Link < ActiveRecord::Base
   
   belongs_to :timestamp
   
+  def self.search(search)
+    if search
+      find(:all, :conditions => ['title LIKE ?', "%#{search}%"])
+    else
+      find(:all)
+    end
+  end
+  
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,16 +2,25 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 $(document).ready(function(){
-  
-  $("#mainSlider").change(function(){
-    sliderVal = $(this).val();
-    $.get("/timelapse/" + sliderVal + ".json", function(data){
-      $("#currentDate").text(data.timestamp);
-      
-      $("#links").empty();
-      $(data.links).each(function(){
-        $("#links").append("<tr><td class='title'>" + this.link.rank + "</td><td class='title'><a href='" + this.link.href + "'>" + this.link.title + "</a></td><td><a href='" + this.link.comments + "'>Comments</a></td></tr>")
+    
+  var sliderMax = $("#slider").attr("data-max");    
+  $("#slider").slider({
+    min: sliderMax - 95,
+    max: sliderMax,
+    value: sliderMax,
+    step: 1, 
+    change: function(event, ui){
+      sliderVal = ui.value;
+     
+      $.get("/timelapse/" + sliderVal + ".json", function(data){
+        $("#currentDate").text(data.timestamp);
+
+        $("#links").empty();
+        $(data.links).each(function(){
+          $("#links").append("<div class='link'><div class='rank'>" + this.link.rank + "</div><a class='title' href='" + this.link.href + "'>" + this.link.title + "</a><a class='comments' href='" + this.link.comments + "'>Comments</a></div>")
+        });
       });
-    });
+    }
   });
+  
 });
